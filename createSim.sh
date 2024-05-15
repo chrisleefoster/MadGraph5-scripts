da58ef72e4c37d101fbcc4bae2e8ded95a2281ab
chrass <chrislfoster96@comcast.net>
2024-05-15 19:23:33 -0400

    Prints error message for incorrect selection

--- createSim.sh
+++ createSim.sh
@@ -4,13 +4,19 @@
 #tar xf templates.tar.gz
 cd templates/
 
-# ASK USER IF THEY WANT AN AVAILABLE TEMPLATE OR BLANK TEMPLATE
+# user is asked to select one of the scripts in /templates/
+#   if a correct selection is made, that script is ran
+#   otherwise, Error message is printed
 echo Choose a template.
 select file_name in *;
 do
     echo You picked $file_name \($REPLY\)
-    echo New simulation folder added to /sims/
-    ./$file_name
+    if [ -n "$file_name" ]; then    
+        ./$file_name
+        echo New simulation folder added to /sims/
+    else
+        echo Please enter the number of an availble template. Try again.
+    fi
     break;
 done
 
